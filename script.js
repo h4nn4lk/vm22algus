8af6fb7326a3b361d36328bac4b406df38c165dd
h4nn4lk <145554031+h4nn4lk@users.noreply.github.com>
2023-12-14 17:15:35 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-console.log('hello world');
-let num = 1;
-num = 3;
-num = 3 * 3 + 7 / 2 - 15;
-//var text = 'hello'; dont use var
-console.log(num);
-let string = 'hello'
-string = "hello dont"
-string = `hello
-is multi line
-string`
-string = 'hello number is: $(num)';
-string = 'hello number is:' + num;
+function calculatePythagorean() {
+    var sideA = parseFloat(document.getElementById('sideA').value);
+    var sideB = parseFloat(document.getElementById('sideB').value);
+    var stepsDiv = document.getElementById('steps');
+
+    if (!isNaN(sideA) && !isNaN(sideB)) {
+        var step1 = "Step 1: Square the length of side A: " + sideA + "² = " + Math.pow(sideA, 2);
+        var step2 = "Step 2: Square the length of side B: " + sideB + "² = " + Math.pow(sideB, 2);
+        var step3 = "Step 3: Add the squared values: " + Math.pow(sideA, 2) + " + " + Math.pow(sideB, 2) + " = " + (Math.pow(sideA, 2) + Math.pow(sideB, 2));
+        var result = "The length of the hypotenuse (side C) is √(" + (Math.pow(sideA, 2) + Math.pow(sideB, 2)) + ") = " + Math.sqrt(Math.pow(sideA, 2) + Math.pow(sideB, 2)).toFixed(2);
+
+        stepsDiv.innerHTML = "<p>" + step1 + "</p><p>" + step2 + "</p><p>" + step3 + "</p>";
+        document.getElementById('result').innerHTML = result;
+    } else {
+        stepsDiv.innerHTML = "";
+        document.getElementById('result').innerHTML = "Please enter valid numbers for both sides.";
+    }
+}
